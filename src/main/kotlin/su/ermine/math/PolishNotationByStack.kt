b96bdf29b85d61dc8b62c3ddc56e38e0ea678c3d
Konstantin Bochkarev <bochkarev@haulmont.com>
2019-07-16 11:27:44 +0400

    added test and found some problem

---
+++ src/main/kotlin/su/ermine/math/PolishNotationByStack.kt
@@ -0,0 +1,93 @@
+package su.ermine.math
+
+import java.util.*
+
+class PolishNotationByStack {
+    private val priority = mapOf('+' to 1, '-' to 1, '*' to 2, '/' to 2)
+    private val tokens : Stack<String> = Stack()
+    private val operators : Stack<Char> = Stack()
+
+    companion object {
+        @JvmStatic fun main(args : Array<String>) {
+            for (arg in args) {
+                val app  = PolishNotationByStack()
+                app.calc(arg)
+            }
+
+            val app  = PolishNotationByStack()
+            app.calc("7 - 2 * 4 + 3")
+        }
+    }
+
+    fun calc(function : String) : String{
+        for (c in function.replace(" ", "").toCharArray()) {
+            when (c) {
+                '(' -> operators.push(c)
+                ')' -> {
+                    while (!operators.empty()) {
+                        val op = operators.pop()
+                        if (op == '(') {
+                            break
+                        }
+                        val op2 = tokens.pop().toInt()
+                        val op1 = tokens.pop().toInt()
+                        tokens.push(operation(op, op1, op2))
+                    }
+                }
+                '+', '-', '/', '*' -> action(c)
+                else -> {
+                    tokens.push(
+                        if (!tokens.empty()) { tokens.pop() } else { "" } + c.toString()
+                    )
+                }
+            }
+        }
+
+        while (!operators.empty()) {
+            val op2 = tokens.pop().toInt()
+            val op1 = tokens.pop().toInt()
+            val op = operators.pop()
+            tokens.push(operation(op, op1, op2))
+        }
+
+        println("************* $function *******************")
+
+        var result = "NaN"
+        while (!tokens.isEmpty()) {
+            result = tokens.pop()
+        }
+
+        println("Result: $result")
+
+        return result
+    }
+
+    private fun check(operator1 : Char, operator2 : Char) : Boolean {
+        return priority.getOrDefault(operator1, 0) >= priority.getOrDefault(operator2, 0) || operator2 == ')'
+    }
+
+    private fun operation(operator : Char, operand1 : Int, operand2 : Int) : String {
+        return when (operator) {
+            '+' -> (operand1 + operand2).toString()
+            '-' -> (operand1 - operand2).toString()
+            '/' -> (operand1 / operand2).toString()
+            '*' -> (operand1 * operand2).toString()
+            else -> ""
+        }
+    }
+
+    private fun action(c : Char) {
+        while (!operators.empty()) {
+            if (!operators.empty() && check(operators.peek(), c)) {
+                val op2 = tokens.pop().toInt()
+                val op1 = tokens.pop().toInt()
+                val op = operators.pop()
+                tokens.push(operation(op, op1, op2))
+            } else {
+                break
+            }
+        }
+        tokens.push("")
+        operators.push(c)
+    }
+}
---
+++ src/test/kotlin/su/ermine/math/PolishNotationByStackKtTest.kt
@@ -0,0 +1,29 @@
+package su.ermine.math
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class PolishNotationByStackKtTest {
+
+    @Test
+    @DisplayName("Calculation test")
+    fun testCalc() {
+        /*val result1 = PolishNotationByStack.calc("(22 + 31 - 3) * (45 - 23 / (2 - 1))")
+        assertEquals("1100", result1)
+
+        val result2 = PolishNotationByStack.calc("(22 + 31 * 3) * (45 - 24 / (2 - 1) * 2) - 5")
+        assertEquals("-350", result2)
+
+        val result3 = PolishNotationByStack.calc("(6 + 10 - 4) / ( 1 + 1 * 2) + 1")
+        assertEquals("5", result3)
+
+        val result4 = PolishNotationByStack.calc("((1 + 2) * 4) + 3")
+        assertEquals("15", result4)*/
+
+/*
+        val result5 = PolishNotationByStack.calc("7 - 2 * 4 + 3")
+        assertEquals("15", result5)
+*/
+    }
+}
