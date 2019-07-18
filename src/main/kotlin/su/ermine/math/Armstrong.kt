1b69369254c048f1123a4d8811af99eeca2f1f7e
Konstantin Bochkarev <ermine.dev@gmail.com>
2019-07-19 00:05:13 +0400

    Start Armstrong algorithm implementation

--- src/main/kotlin/su/ermine/math/Armstrong.kt
+++ src/main/kotlin/su/ermine/math/Armstrong.kt
@@ -1,5 +1,7 @@
 package su.ermine.math
 
+import java.lang.Math.pow
+
 fun main() {
     testArmstrongNumbers()
 }
@@ -32,16 +34,52 @@ fun testArmstrongNumbers () : String {
     println("Count = $count")
 
     for (n in 0..count) {
+        //println(pow(10.0, n.toDouble()))
+        find(n)
+    }
+
+/*
+    for (n in 0..count) {
         for (z in n..9) {
             for (k in z..9) {
                 println("Value = $n$z$k")
             }
         }
     }
+*/
 
     val end = System.currentTimeMillis()
     val sec = (end - start) / 1000f
     println("$sec seconds")
 
     return ""
+}
+
+/*
+    При использовании конструктора нужно указать размер массива в первом параметре и лямбда-выражение во втором.
+
+
+    val myArray = Array(5, { i -> i * 2 })
+    println(myArray[3])
+    Мы задали пять элементов и каждый элемент в цикле умножаем на 2. В итоге получим массив чисел 0, 2, 4, 6, 8.
+
+    Создадим массив строк от "A" до "Z"
+
+
+    val letters = Array<String>(26) { i -> ('A' + i).toString() }
+    println(letters.joinToString(""))
+    Лямбда-выражение принимает индекс элемента массива и возвращает значение, которое будет помещено в массив с этим индексом. Значение вычисляется путём сложения индекса с кодом символа и преобразованием результата в строку.
+*/
+
+fun find(n : Int) {
+    if (n == 0) return
+
+    val arr = IntArray(n) {i -> i + 1}
+
+    print("Массив #$n")
+    for (i in arr) {
+        print("\t $i")
+    }
+
+    println()
 }
